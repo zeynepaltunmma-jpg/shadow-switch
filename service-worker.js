1ffcfd73c5d5cdfdfd9d12dcf4a21739cd8b620c
zeynep altun <zeynepaltunmma@gmail.com>
2026-07-18 23:54:54 +0300

    Add working service worker

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,55 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "shadow-switch-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(APP_FILES);
+        })
+    );
+
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames.map((name) => caches.delete(name))
-                );
-            })
-            .then(() => self.registration.unregister())
-            .then(() => self.clients.matchAll())
-            .then((clients) => {
-                clients.forEach((client) => {
-                    client.navigate(client.url);
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const responseCopy = response.clone();
+
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseCopy);
                 });
+
+                return response;
             })
+            .catch(() => caches.match(event.request))
     );
 });
