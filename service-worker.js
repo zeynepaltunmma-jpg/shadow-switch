ae90b314f81cf88de6d6cfc8c0e09dc53b2c1e69
zeynep altun <zeynepaltunmma@gmail.com>
2026-07-18 23:50:33 +0300

    Fix PWA

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,21 @@
-const CACHE_NAME = "shadow-switch-v2";
-
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "./assets/audio/switch.mp3",
-    "./assets/audio/hit.mp3",
-    "./icons/icon-192.png",
-    "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-
+self.addEventListener("install", () => {
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames
-                    .filter((name) => name !== CACHE_NAME)
-                    .map((name) => caches.delete(name))
-            );
-        })
-    );
-
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
+        caches.keys()
+            .then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.map((name) => caches.delete(name))
+                );
+            })
+            .then(() => self.registration.unregister())
+            .then(() => self.clients.matchAll())
+            .then((clients) => {
+                clients.forEach((client) => {
+                    client.navigate(client.url);
+                });
+            })
     );
 });
